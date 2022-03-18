ddece8288e0e8c8b724cb0f26ca4c7dc6119695d
GustafJ <gustaf89@hotmail.se>
2022-03-18 16:07:18 +0100

    add initiall button styling

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -3,16 +3,10 @@
 </script>
 
 <template>
-  <button class="btn">
+  <button class="btn btn--hollow">
     <slot></slot>
   </button>
 </template>
 
 <style lang="scss">
-  .btn {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
 </style>
