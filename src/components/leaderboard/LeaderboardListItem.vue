66daaa468489e97e93dda125a24b99e789694a24
GustafJ <gustaf89@hotmail.se>
2022-03-21 09:35:51 +0100

    style leaderboard

--- src/components/leaderboard/LeaderboardListItem.vue
+++ src/components/leaderboard/LeaderboardListItem.vue
@@ -1,17 +1,23 @@
 <script setup>
 
+const props = defineProps({
+  post: {
+    type: Object,
+  }
+});
+
+console.log(props.post);
+
 </script>
 
 <template>
   <li class="leaderboard--listitem">
     <div class="leaderboard--listitem-flex">
       <div class="leaderboard--listitem-main">
-        <h3 class="leaderboard--listitem-author">Gustaf Johnsson</h3>
-        <p
-          class="leaderboard--listitem-post"
-        >Biscuit oat cake donut danish chupa chups. Oat cake gummies halvah candy muffin candy cupcake jelly-o cupcake. Biscuit toffee carrot cake bear claw donut biscuit. Jelly marzipan chupa chups soufflé caramels pie icing bear claw. Liquorice candy canes cake gummies toffee. Ice cream carrot cake marshmallow dessert soufflé cookie.</p>
+        <h3 class="leaderboard--listitem-author">{{ post.author }}</h3>
+        <p class="leaderboard--listitem-post">{{ post.post }}</p>
       </div>
-      <div class="leaderboard--listitem-tier">17</div>
+      <div class="leaderboard--listitem-tier">{{ post.tier }}</div>
     </div>
   </li>
 </template>
--- src/pages/LeaderboardPage.vue
+++ src/pages/LeaderboardPage.vue
@@ -2,7 +2,28 @@
 import LeaderboardList from "../components/leaderboard/LeaderboardList.vue";
 import LeaderboardListItem from "../components/leaderboard/LeaderboardListItem.vue";
 
-const hejsan = [1, 2, 3, 4, 5, 6];
+class Post {
+  author;
+  post;
+  tier;
+  id = Math.random();
+
+  constructor(author, post, tier) {
+    this.author = author;
+    this.post = post;
+    this.tier = tier;
+  }
+}
+
+const posts = [
+  new Post('Reginald Rivera', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 100),
+  new Post('Beth Jordan', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 80),
+  new Post('Lena Tores', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 74),
+  new Post('Sara Reynolds', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 73),
+  new Post('Brad Cook', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 20),
+  new Post('Pearl Prescott', 'Apple pie lollipop powder sesame snaps gingerbread macaroon muffin. Wafer wafer tiramisu cake cupcake. Tart wafer croissant wafer jelly beans.', 19),
+
+];
 
 </script>
 
@@ -11,7 +32,7 @@ const hejsan = [1, 2, 3, 4, 5, 6];
     <div class="leaderboard--wrapper">
       <h2 class="leaderboard--title">Leaderboard</h2>
       <LeaderboardList class="leaderboard--list">
-        <LeaderboardListItem v-for="hej in hejsan" :key="hej"></LeaderboardListItem>
+        <LeaderboardListItem v-for="post in posts" :key="post.id" :post="post" />
       </LeaderboardList>
     </div>
   </div>
